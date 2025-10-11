83d33484dee05f31a34353dda2fc353541ebccec
Yago Garzon Chaves <114442039+yagogarzon@users.noreply.github.com>
2025-10-11 20:48:22 -0300

    Atualizacao UML

--- BackEnd/backend/src/main/java/com/calculo_folha_pagamento_lab1_grupo5/backend/model/iCalculoAdicionais.java
+++ BackEnd/backend/src/main/java/com/calculo_folha_pagamento_lab1_grupo5/backend/model/iCalculoAdicionais.java
@@ -2,6 +2,6 @@ package com.calculo_folha_pagamento_lab1_grupo5.backend.model;
 
 public interface iCalculoAdicionais{
 
-    double calcularAdicionais(Funcionario x);
+    double calcularAdicionais(Funcionario f);
     
 }
